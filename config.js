f8c5bd1a94bfb788d13f9b72462f2e64a24b7aee
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-11-14 22:29:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-
 module.exports = {
   telegramBotTokens: [
-    process.env.7015056498:AAGcZa04jeVe9EvsjlfW_6P2k95D57sL8cY,
-    process.env.6443871444:AAEqas0L2bJQtybywpDuq3zcycWpiZV83vo,
-    process.env.7463542222:AAF14OHfN8mh5rstHUI5L6IZRITjHahBJYQ,
+    "7015056498:AAGcZa04jeVe9EvsjlfW_6P2k95D57sL8cY",  // Bot token 1
+    "6443871444:AAEqas0L2bJQtybywpDuq3zcycWpiZV83vo",  // Bot token 2
+    "7463542222:AAF14OHfN8mh5rstHUI5L6IZRITjHahBJYQ",  // Bot token 3
     // Add more tokens as needed
   ],
   serverPort: 8000
