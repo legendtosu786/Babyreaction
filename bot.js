32dbc456897514b986f33bac274fab58abb536e3
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-12-02 15:12:25 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ bot.on('message', (msg) => {
     const doEmoji = myEmoji[Math.floor(Math.random() * myEmoji.length)];
 
     // Send the emoji as a reaction using HTTP POST request
-    axios.post(`https://api.telegram.org/bot${token}/setMessageReaction`, {
+    axios.post(`https://api.telegram.org/bot${mainBotToken}/setMessageReaction`, {
       chat_id: chatId,
       message_id: messageId,
       reaction: JSON.stringify([
